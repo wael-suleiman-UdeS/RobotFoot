d786244fb3ea993305e1efa4a57387dd9019e7b6
Robin <empire_9@hotmail.com>
2013-03-07 12:48:01 -0500

    Transmutation du code en orienté objet

---
+++ TraitementImage/Experimentation/MyNewOpenCVProject/MyNewOpenCVProject/test.h
@@ -0,0 +1,22 @@
+#include <opencv\cvaux.h>
+#include <opencv\highgui.h>
+#include <opencv\cxcore.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <math.h>
+#include <float.h>
+#include <limits.h>
+#include <time.h>
+#include <ctype.h>
+
+class test {
+
+public:
+	void testPrimaire();
+	void trackBall();
+private:
+
+};
