f06d04c71241e91b599ccc3ea0a2b1bd456e35c2
James-Adam Renquinha Henri <jarhmander@gmail.com>
2013-03-13 08:09:34 -0400

    Encore plus près du but! Linker script et startup à modifier encore...

--- cartectrl/cartectrl/src/main.c
+++ cartectrl/cartectrl/src/main.c
@@ -1,17 +1,13 @@
-#include <stm32f4xx.h>
-#include <stm32f4xx_gpio.h>
-#include <stm32f4xx_flash.h>
-#include <stm32f4xx_rcc.h>
-#include <misc.h>
 #include "usbd_cdc_core.h"
 #include "usbd_usr.h"
 #include "usbd_desc.h"
 #include "usbd_cdc_vcp.h"
 
+//Library config for this project!!!!!!!!!!!
 #include "stm32f4xx_conf.h"
 
 
-USB_OTG_CORE_HANDLE  USB_OTG_dev __attribute__ ((aligned (4)));
+__ALIGN_BEGIN USB_OTG_CORE_HANDLE  USB_OTG_dev __ALIGN_END;
 
 void Delay(volatile uint32_t nCount) {
   while(nCount--) {
@@ -94,7 +90,6 @@ void initClock(void)
   /* At this stage the HSI is already enabled */
 
   /* Enable Prefetch Buffer and set Flash Latency */
-  #if 0
   FLASH_SetLatency(FLASH_Latency_5);
   FLASH_PrefetchBufferCmd(ENABLE);
 
@@ -116,148 +111,34 @@ void initClock(void)
 
   RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);
 
-  #elseif 0
-  RCC->CFGR = 0x00009400;
-
-  RCC->PLLCFGR = 0x04005410;
-  /* Enable PLL */
-  RCC->CR |= RCC_CR_PLLON;
-
-
-  /* Wait till PLL is ready */
-  while((RCC->CR & RCC_CR_PLLRDY) == 0)
-  {
-  }
-
-  /* Select PLL as system clock source */
-  RCC->CFGR &= (uint32_t)((uint32_t)~(RCC_CFGR_SW));
-  RCC->CFGR |= (uint32_t)RCC_CFGR_SW_PLL;
-
-  /* Wait till PLL is used as system clock source */
   while ((RCC->CFGR & (uint32_t)RCC_CFGR_SWS) != (uint32_t)RCC_CFGR_SWS_PLL)
   {
   }
-  #else
 
-  /* PLL_VCO = (HSE_VALUE or HSI_VALUE / PLL_M) * PLL_N */
-#define PLL_M      8
-#define PLL_N      336
 
-/* SYSCLK = PLL_VCO / PLL_P */
-#define PLL_P      2
-
-/* USB OTG FS, SDIO and RNG Clock =  PLL_VCO / PLLQ */
-#define PLL_Q      7
-
-/* PLLI2S_VCO = (HSE_VALUE Or HSI_VALUE / PLL_M) * PLLI2S_N
-   I2SCLK = PLLI2S_VCO / PLLI2S_R */
-#define PLLI2S_N   192
-#define PLLI2S_R   5
-
-   /* Reset the RCC clock configuration to the default reset state ------------*/
-  /* Set HSION bit */
-  RCC->CR |= (uint32_t)0x00000001;
-
-  /* Reset CFGR register */
-  RCC->CFGR = 0x00000000;
-
-  /* Reset HSEON, CSSON and PLLON bits */
-  RCC->CR &= (uint32_t)0xFEF6FFFF;
-
-  /* Reset PLLCFGR register */
-  RCC->PLLCFGR = 0x24003010;
-
-  /* Reset HSEBYP bit */
-  RCC->CR &= (uint32_t)0xFFFBFFFF;
-
-  /* Disable all interrupts */
-  RCC->CIR = 0x00000000;
-
-    __IO uint32_t StartUpCounter = 0, HSEStatus = 0;
-
-  /* Enable HSE */
-  RCC->CR |= ((uint32_t)RCC_CR_HSEON);
-
-  /* Wait till HSE is ready and if Time out is reached exit */
-  do
-  {
-    HSEStatus = RCC->CR & RCC_CR_HSERDY;
-    StartUpCounter++;
-  } while((HSEStatus == 0) && (StartUpCounter != HSE_STARTUP_TIMEOUT));
-
-  if ((RCC->CR & RCC_CR_HSERDY) != RESET)
-  {
-    HSEStatus = (uint32_t)0x01;
-  }
-  else
-  {
-    HSEStatus = (uint32_t)0x00;
-  }
-
-  if (HSEStatus == (uint32_t)0x01)
-  {
-    /* Enable high performance mode, System frequency up to 168 MHz */
-    RCC->APB1ENR |= RCC_APB1ENR_PWREN;
-    PWR->CR |= PWR_CR_PMODE;
-
-    /* HCLK = SYSCLK / 1*/
-    RCC->CFGR |= RCC_CFGR_HPRE_DIV1;
-
-    /* PCLK2 = HCLK / 2*/
-    RCC->CFGR |= RCC_CFGR_PPRE2_DIV2;
-
-    /* PCLK1 = HCLK / 4*/
-    RCC->CFGR |= RCC_CFGR_PPRE1_DIV4;
-
-    /* Configure the main PLL */
-    RCC->PLLCFGR = PLL_M | (PLL_N << 6) | (((PLL_P >> 1) -1) << 16) |
-                   (RCC_PLLCFGR_PLLSRC_HSE) | (PLL_Q << 24);
-
-    /* Enable the main PLL */
-    RCC->CR |= RCC_CR_PLLON;
-
-    /* Wait till the main PLL is ready */
-    while((RCC->CR & RCC_CR_PLLRDY) == 0)
-    {
-    }
-
-    /* Configure Flash prefetch, Instruction cache, Data cache and wait state */
-    FLASH->ACR = FLASH_ACR_ICEN |FLASH_ACR_DCEN |FLASH_ACR_LATENCY_5WS;
-
-    /* Select the main PLL as system clock source */
-    RCC->CFGR &= (uint32_t)((uint32_t)~(RCC_CFGR_SW));
-    RCC->CFGR |= RCC_CFGR_SW_PLL;
-
-    /* Wait till the main PLL is used as system clock source */
-    while ((RCC->CFGR & (uint32_t)RCC_CFGR_SWS ) != RCC_CFGR_SWS_PLL);
-    {
-    }
-  }
-  else
-  { /* If HSE fails to start-up, the application will have wrong clock
-         configuration. User can add here some code to deal with this error */
-  }
-
-  #endif
 }
 
 int main(void){
+  int len;
+  uint8_t buf[255];
 
   // initialize the GPIO pins we need
   initClock();
+  USBD_Init(&USB_OTG_dev,
+            USB_OTG_FS_CORE_ID,
+            &USR_desc,
+            &USBD_CDC_cb,
+            &USR_cb);
+  for (;;)
+  {
+
+        len = VCP_get_string(&buf[0]);
+		if(len)
+		    VCP_send_str(&buf[0]);
+
+  }
   init_GPIO();
-  int len;
-  uint8_t buf[255];
-  /* This flashed the LEDs on the board once
-   * Two registers are used to set the pins (pin level is VCC)
-   * or to reset the pins (pin level is GND)
-   *
-   * BSRR stands for bit set/reset register
-   * it is seperated into a high and a low word (each of 16 bit size)
-   *
-   * A logical 1 in BSRRL will set the pin and a logical 1 in BSRRH will
-   * reset the pin. A logical 0 in either register has no effect
-   */
+
   int i=0;
   for (;i<4;++i)
   {
@@ -267,12 +148,9 @@ int main(void){
       Delay(10000000L);
   }
 //*
-   USBD_Init(&USB_OTG_dev,
-            USB_OTG_FS_CORE_ID,
-            &USR_desc,
-            &USBD_CDC_cb,
-            &USR_cb);
+
             //*/
+  for (;;);
   GPIOD->BSRRL = 0x2000;
 
   //int x = 0;
@@ -305,4 +183,15 @@ int main(void){
 		Delay(DELAYELEM);
 	}
 	*/
+}
+
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
 }
