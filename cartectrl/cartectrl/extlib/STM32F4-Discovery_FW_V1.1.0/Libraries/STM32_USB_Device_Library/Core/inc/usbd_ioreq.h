af57040922165b3823d753d7ae7c113104f1c156
James-Adam Renquinha Henri <jarhmander@gmail.com>
2013-03-14 17:24:51 -0400

    Ajout de extern "C". Retrait du code de vcp.

--- cartectrl/cartectrl/extlib/STM32F4-Discovery_FW_V1.1.0/Libraries/STM32_USB_Device_Library/Core/inc/usbd_ioreq.h
+++ cartectrl/cartectrl/extlib/STM32F4-Discovery_FW_V1.1.0/Libraries/STM32_USB_Device_Library/Core/inc/usbd_ioreq.h
@@ -17,12 +17,16 @@
   *
   * <h2><center>&copy; COPYRIGHT 2011 STMicroelectronics</center></h2>
   ******************************************************************************
-  */ 
+  */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
 
 #ifndef __USBD_IOREQ_H_
 #define __USBD_IOREQ_H_
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
 
 /* Includes ------------------------------------------------------------------*/
 #include  "usbd_def.h"
@@ -31,18 +35,18 @@
 /** @addtogroup STM32_USB_OTG_DEVICE_LIBRARY
   * @{
   */
-  
+
 /** @defgroup USBD_IOREQ
   * @brief header file for the usbd_ioreq.c file
   * @{
-  */ 
+  */
 
 /** @defgroup USBD_IOREQ_Exported_Defines
   * @{
-  */ 
+  */
 /**
   * @}
-  */ 
+  */
 
 
 /** @defgroup USBD_IOREQ_Exported_Types
@@ -52,64 +56,67 @@
 
 /**
   * @}
-  */ 
+  */
 
 
 
 /** @defgroup USBD_IOREQ_Exported_Macros
   * @{
-  */ 
+  */
 
 /**
   * @}
-  */ 
+  */
 
 /** @defgroup USBD_IOREQ_Exported_Variables
   * @{
-  */ 
+  */
 
 /**
   * @}
-  */ 
+  */
 
 /** @defgroup USBD_IOREQ_Exported_FunctionsPrototype
   * @{
-  */ 
+  */
 
-USBD_Status  USBD_CtlSendData (USB_OTG_CORE_HANDLE  *pdev, 
+USBD_Status  USBD_CtlSendData (USB_OTG_CORE_HANDLE  *pdev,
                                uint8_t *buf,
                                uint16_t len);
 
-USBD_Status  USBD_CtlContinueSendData (USB_OTG_CORE_HANDLE  *pdev, 
+USBD_Status  USBD_CtlContinueSendData (USB_OTG_CORE_HANDLE  *pdev,
                                uint8_t *pbuf,
                                uint16_t len);
 
-USBD_Status USBD_CtlPrepareRx (USB_OTG_CORE_HANDLE  *pdev, 
-                               uint8_t *pbuf,                                 
+USBD_Status USBD_CtlPrepareRx (USB_OTG_CORE_HANDLE  *pdev,
+                               uint8_t *pbuf,
                                uint16_t len);
 
-USBD_Status  USBD_CtlContinueRx (USB_OTG_CORE_HANDLE  *pdev, 
-                              uint8_t *pbuf,                                          
+USBD_Status  USBD_CtlContinueRx (USB_OTG_CORE_HANDLE  *pdev,
+                              uint8_t *pbuf,
                               uint16_t len);
 
 USBD_Status  USBD_CtlSendStatus (USB_OTG_CORE_HANDLE  *pdev);
 
 USBD_Status  USBD_CtlReceiveStatus (USB_OTG_CORE_HANDLE  *pdev);
 
-uint16_t  USBD_GetRxCount (USB_OTG_CORE_HANDLE  *pdev , 
+uint16_t  USBD_GetRxCount (USB_OTG_CORE_HANDLE  *pdev ,
                            uint8_t epnum);
 
+#ifdef __cplusplus
+  }
+#endif
 /**
   * @}
-  */ 
+  */
 
 #endif /* __USBD_IOREQ_H_ */
 
 /**
   * @}
-  */ 
+  */
 
 /**
 * @}
-*/ 
+*/
 /******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
