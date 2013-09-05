1ab5266bc81e3fc0a32cac4e59a52a563a9671e4
ProjetS7S8-20 <m.drapeau9@gmail.com>
2013-09-05 11:52:49 -0400

    Added Eigen library and motor calcul for static walking demo.

---
+++ Embedded Computer/source/Demo/StaticWalking/matrixpinv.h
@@ -0,0 +1,16 @@
+// APP GEI 744, Copyright 2013, Wael Suleiman
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Eigen/Dense"
+#include "Eigen/Core"
+#include "Eigen/SVD"
+using namespace Eigen;
+/**
+ * DLS  of the linear equation: Ax=b
+ * DLS inverse of a matrix 
+ */
+
+MatrixXd DLS_LE(MatrixXd& A, VectorXd& b);
+
+MatrixXd DLS_inverse(MatrixXd& A, double epsilon= 1E-4);
