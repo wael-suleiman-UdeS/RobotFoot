598d1019748a4e8999427646903c93731f79c798
Mitchel <mitchellabonte@hotmail.com>
2013-10-15 10:20:04 -0700

    fixed

--- Embedded_Computer/src/ImageProcessing/ObjectTracker.cpp
+++ Embedded_Computer/src/ImageProcessing/ObjectTracker.cpp
@@ -27,7 +27,7 @@ void ObjectTracker::initializeHack(const XmlParser& config)
 	_controller->setTorque(_tilt, STM32F4::TorqueOn);
 }
 
-void ObjectTracker::initializeHack(const XmlParser& config) {
+void ObjectTracker::initializeHackPID(const XmlParser& config) {
 
 	path basePath = XmlPath::Root / XmlPath::Motion / XmlPath::Motors / XmlPath::Head / "PID";
 
